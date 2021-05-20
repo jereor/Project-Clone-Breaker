32a047ff06845b60445d216bcf806c139a1bf16b
jereor <jere.orava@hotmail.com>
2021-05-20 22:58:01 +0300

    Added randomized audio to when the ball hits something.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,14 +6,23 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] GameObject paddle;
+    [SerializeField] AudioClip[] ballSounds;
     
     Vector2 paddleToBallVector;
     public bool ballLocked = true;
+
+    AudioSource hitSound;
+    AudioSource cloneSound;
+
+    AudioSource aSource;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle.transform.position;
+        aSource = GetComponent<AudioSource>();
+        hitSound = transform.GetChild(0).GetComponent<AudioSource>();
+        cloneSound = transform.GetChild(1).GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,5 +38,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!ballLocked)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            aSource.PlayOneShot(clip);
+        }
     }
 }
