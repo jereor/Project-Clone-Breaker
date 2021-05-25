0c5b0451ab1109011bf3683910634d22dbe0a3dc
jereor <jere.orava@hotmail.com>
2021-05-25 20:09:04 +0300

    Damaged blocks visualized

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +7,32 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     // Config parameters
+    [SerializeField] AudioClip hitClip;
     [SerializeField] AudioClip destroyClip;
+    [SerializeField] AudioClip clinkClip;
     [SerializeField] GameObject blockSparklesVFX;
     [SerializeField] int maxHits;
+    [SerializeField] Sprite[] hitSprites;
 
     // Cached references
     LevelManager levelManager;
     GameStatus gameStatus;
+    public SpriteRenderer spriteRenderer;
 
     // State variables
     [SerializeField] int timesHit;
 
     private void Start()
     {
+        InitializeCachedReferences();
+        CountToBlocks();
+    }
+
+    private void InitializeCachedReferences()
+    {
         gameStatus = FindObjectOfType<GameStatus>();
         levelManager = FindObjectOfType<LevelManager>();
-        CountToBlocks();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void CountToBlocks()
@@ -31,11 +42,24 @@ public class Block : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (tag == "Breakable")
-        {
-            if (timesHit++ >= maxHits)
-                DestroyBlock();
-        }
+        if (tag == "Breakable") HandleHit();
+        else AudioSource.PlayClipAtPoint(clinkClip, Camera.main.transform.position);
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+            DestroyBlock();
+        else
+            DamageBlock();
+    }
+
+    private void DamageBlock()
+    {
+        AudioSource.PlayClipAtPoint(hitClip, Camera.main.transform.position);
+        int spriteIndex = timesHit - 1;
+        spriteRenderer.sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
